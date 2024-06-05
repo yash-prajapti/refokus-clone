f14325a994573f0842e86c929d2f42e957673639
yash-prajapti <yashhprajapatiii@gmail.com>
2024-06-05 17:12:33 +0530

    loader animation change

--- src/Components/Lazyloader/Lazyloader.jsx
+++ src/Components/Lazyloader/Lazyloader.jsx
@@ -2,18 +2,19 @@ import React, { useEffect } from 'react'
 import './Lazyloader.css'
 import './n.scss'
 import gsap from 'gsap';
+import { RingLoader } from 'react-spinners';
 
 function Lazyloader({ name, loadingState }) {
     useEffect(() => {
         let Tl = gsap.timeline();
-        if(!loadingState){
-        Tl.to('.loader-container', {
-            top: '-100%',
-            delay: 1,
-            duration: 2,
-            ease: "expo.inOut",
-        });
-    }
+        if (!loadingState) {
+            Tl.to('.loader-container', {
+                top: '-100%',
+                delay: 1,
+                duration: 2,
+                ease: "expo.inOut",
+            });
+        }
 
         // Cleanup function to ensure GSAP kills the animation when the component unmounts
         return () => {
@@ -25,6 +26,14 @@ function Lazyloader({ name, loadingState }) {
             <div className='portfolio-experiment'>
                 <div className="experiment-title">{name}</div>
             </div>
+            {loadingState ? (<div className='loader-rs'>
+                <span className='loader-text'>Slow connection</span>
+                <RingLoader color="#3A1675" />
+            </div>) : (<div className='loader-rs'>
+                <span className='loader-text'>Established</span>
+                <RingLoader color="#3A1675" />
+            </div>)}
+
         </div>
     )
 }
